9716c987282dacac3f76f51983b7a4258b65dbc7
mikertillman <153720776+mikertillman@users.noreply.github.com>
2024-03-27 17:03:26 -0500

    Update DBManage.js

--- Backend/DBManage.js
+++ Backend/DBManage.js
@@ -25,7 +25,7 @@ const sqlite3 = require('sqlite3').verbose();
 //     });
 // });
 
-const db = new sqlite3.Database(DATABASE. sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database(DATABASE,sqlite3.OPEN_READWRITE,(err) => {
     if (err) return console.error(err.message);
 });
 
